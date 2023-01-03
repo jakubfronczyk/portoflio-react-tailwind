d0059d18c0483e84d429b1dfb53598350d52a4c6
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-01-03 11:37:37 +0100

    added contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {  Fragment, useState } from 'react'
 import Header from './components/Header/Header'
 import First from './components/First/First'
 import About from './components/About/About'
-import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 import classes from './App.module.scss'
@@ -17,7 +16,6 @@ function App() {
       <div className={classes.wrapper}>
         <First />
         <About />
-        <Skills />
         <Projects />
         <Contact />
       </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,49 @@ const Contact = () => {
     return (
         <div className={classes.contact}>
             <div className={classes.contact__content}>
-
+                <div className={classes.contact__content__form}>
+                    <div className={classes.contact__content__form__article}>
+                        <h1>Send me a message.</h1>
+                        <p>Got a question or proposal? Go ahead.</p>
+                    </div>
+                    <form action="" method="post" className={classes.contact__content__form__inner}>
+                        <div className={classes.contact__content__form__inner__personal}>
+                            <div className={classes.contact__content__form__inner__personal__name}>
+                                <label htmlFor="">Your Name</label>
+                                <input type="text" placeholder="Enter your name" required="required"/>
+                            </div>
+                            <div className={classes.contact__content__form__inner__personal__email}>
+                                <label htmlFor="">Email Address</label>
+                                <input type="text" placeholder="Enter your email address" required="required"/>
+                            </div>
+                        </div>
+                        <div className={classes.contact__content__form__inner__message}>
+                            <div className={classes.contact__content__form__inner__message__inner}>
+                                <label htmlFor="">Your Message</label>
+                                <input type="text" placeholder="Enter your message" required="required"/>
+                            </div>
+                        </div>
+                        <button>Shoot</button>
+                    </form>
+                </div>
+                <div className={classes.contact__content__links}>
+                    <div>
+                    <a href="/">EMAIL@GMAIL.COM</a>
+                    </div>
+                    <div>
+                        <ol>
+                            <li>
+                                <a href="/">TW</a>
+                            </li>
+                            <li>
+                                <a href="/">LN</a>
+                            </li>
+                            <li>
+                                <a href="/">GH</a>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
             </div>
         </div>
     )
